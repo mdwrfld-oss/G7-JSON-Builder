f751e375e68f45942023c34597c469a412dc27bc
mdwrfld-oss <matt.weiner@g7marketing.com>
2025-12-06 19:24:15 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // This is critical for GitHub Pages. It ensures assets use relative paths.
-  base: './', 
+  base: '/G7-JSON-Builder/', 
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   }
